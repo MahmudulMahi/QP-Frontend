d7c63a0b514c64875a808c4284cb94be79509ca1
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-07-08 00:29:41 +0600

    create navber for lagre device

--- src/Pages/Common/Navber/Navber.jsx
+++ src/Pages/Common/Navber/Navber.jsx
@@ -1,57 +1,52 @@
 import React from 'react';
+import logo from "../../../assets/navberimg/image 386.png"
+import homebtn from "../../../assets/navberimg/Vector (1).png"
+import navicon1 from "../../../assets/navberimg/Group 1000003376.png"
+import navicon2 from "../../../assets/navberimg/Group_light.png"
+import navicon3 from "../../../assets/navberimg/Group 1000003341.png"
+import navicon4 from "../../../assets/navberimg/Vector (2).png"
+import profile from "../../../assets/navberimg/Group 1000003375.png"
+import messenger from "../../../assets/navberimg/Frame 14.png"
+import notification from "../../../assets/navberimg/Frame 15.png"
+import searchber from "../../../assets/navberimg/Group 1000003374.png"
+import { Link } from 'react-router-dom';
 
 const Navber = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16" />
-            </svg>
-          </div>
+      
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-            <li><a>Item 1</a></li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </li>
+            <li><img src={homebtn} alt="" /></li>
+         
             <li><a>Item 3</a></li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <img className='mr-6' src={logo} alt="" />
+        <img className='hidden lg:block' src={searchber} alt="" />
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li><a>Item 1</a></li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </details>
-          </li>
-          <li><a>Item 3</a></li>
-        </ul>
+      <div className="navbar-center hidden lg:flex ">
+        {/* <ul className=" menu-horizontal px-1 gap-[100] "> */}
+        <div className='flex space-x-[80px] ml-48  '>
+          <Link><img className='w-6 h-6 hidden lg:block' src={homebtn} alt="" /></Link>
+          <Link><img className='w-6 h-6 hidden lg:block' src={navicon1} alt="" /></Link>
+          <Link><img className='w-6 h-6 hidden lg:block' src={navicon2} alt="" /></Link>
+          <Link><img className='w-6 h-6 hidden lg:block' src={navicon3} alt="" /></Link>
+          <Link><img className='w-6 h-6 hidden lg:block' src={navicon4} alt="" /></Link>
+        </div>
+
+
+        {/* </ul> */}
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <div className='flex space-x-4'>
+          <img className='max-w-12 max-h-12 hidden lg:block' src={messenger} alt="" />
+          <img className='max-w-12 max-h-12 hidden lg:block' src={notification} alt="" />
+          <img className='max-w-12 max-h-12 hidden lg:block' src={profile} alt="" />
+        </div>
       </div>
     </div>
   );
