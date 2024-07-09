aba8c03605ff15ffaaa646f2983c1d4e567c4ddb
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-07-10 01:35:10 +0600

    create context and get fiebase authontication element

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext=createContext()
@@ -13,11 +13,31 @@ const AuthProvider = ({children}) => {
     setLoading(true)
     return createUserWithEmailAndPassword(auth, email, password)
   }
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+  useEffect(() => {
+    const unsubscribe=onAuthStateChanged(auth,currentUser =>{
+      setUser(currentUser)
+      console.log("current user",currentUser)
+      setLoading(false)
+    })
+    return ()=>{
+      return unsubscribe()
+    }
+  },[])
 
   const authInfo={
     user,
     loading,
     createUser,
+    signIn,
+    logOut,
 
   }
   return (
